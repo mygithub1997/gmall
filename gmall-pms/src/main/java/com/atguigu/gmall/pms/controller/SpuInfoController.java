6d4b80ff84c633a8c3f89b55a9880b9a72f321f1
qc <872448085@qq.com>
2020-03-07 08:50:58 +0800

    day14RabbitMQ消息队列和单点登录原理

--- gmall-pms/src/main/java/com/atguigu/gmall/pms/controller/SpuInfoController.java
+++ gmall-pms/src/main/java/com/atguigu/gmall/pms/controller/SpuInfoController.java
@@ -12,14 +12,13 @@ import com.atguigu.core.bean.Resp;
 import com.atguigu.gmall.pms.vo.SpuInfoVO;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
+import org.springframework.amqp.core.AmqpTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import com.atguigu.gmall.pms.entity.SpuInfoEntity;
 import com.atguigu.gmall.pms.service.SpuInfoService;
-
-
 
 
 /**
@@ -33,85 +32,88 @@ import com.atguigu.gmall.pms.service.SpuInfoService;
 @RestController
 @RequestMapping("pms/spuinfo")
 public class SpuInfoController {
-    @Autowired
-    private SpuInfoService spuInfoService;
-
-    @GetMapping
-    public Resp<PageVo> querySpuByCidPage(QueryCondition condition, @RequestParam(value = "catId", defaultValue = "0")Long cid){
-        PageVo pageVo = spuInfoService.querySpuByCidPage(condition, cid);
-
-        return Resp.ok(pageVo);
-    }
-
-    /**
-     * 列表
-     */
-    @ApiOperation("分页查询(排序)")
-    @GetMapping("/list")
-    @PreAuthorize("hasAuthority('pms:spuinfo:list')")
-    public Resp<PageVo> list(QueryCondition queryCondition) {
-        PageVo page = spuInfoService.queryPage(queryCondition);
-
-        return Resp.ok(page);
-    }
-
-    /**
-     * 列表
-     */
-    @PostMapping("/page")
-    public Resp<List<SpuInfoEntity>> querySpuPage(@RequestBody QueryCondition queryCondition) {
-        PageVo page = spuInfoService.queryPage(queryCondition);
-
-        return Resp.ok((List<SpuInfoEntity>)page.getList());
-    }
-
-
-    /**
-     * 信息
-     */
-    @ApiOperation("详情查询")
-    @GetMapping("/info/{id}")
-    @PreAuthorize("hasAuthority('pms:spuinfo:info')")
-    public Resp<SpuInfoEntity> info(@PathVariable("id") Long id){
-		SpuInfoEntity spuInfo = spuInfoService.getById(id);
-
-        return Resp.ok(spuInfo);
-    }
-
-    /**
-     * 保存
-     */
-    @ApiOperation("保存")
-    @PostMapping("/save")
-    @PreAuthorize("hasAuthority('pms:spuinfo:save')")
-    public Resp<Object> save(@RequestBody SpuInfoVO spuInfoVO) throws FileNotFoundException {
-		spuInfoService.bigSave(spuInfoVO);
-
-        return Resp.ok(null);
-    }
-
-    /**
-     * 修改
-     */
-    @ApiOperation("修改")
-    @PostMapping("/update")
-    @PreAuthorize("hasAuthority('pms:spuinfo:update')")
-    public Resp<Object> update(@RequestBody SpuInfoEntity spuInfo){
-		spuInfoService.updateById(spuInfo);
-
-        return Resp.ok(null);
-    }
-
-    /**
-     * 删除
-     */
-    @ApiOperation("删除")
-    @PostMapping("/delete")
-    @PreAuthorize("hasAuthority('pms:spuinfo:delete')")
-    public Resp<Object> delete(@RequestBody Long[] ids){
-		spuInfoService.removeByIds(Arrays.asList(ids));
-
-        return Resp.ok(null);
-    }
-
+        @Autowired
+        private SpuInfoService spuInfoService;
+        
+        @Autowired
+        private AmqpTemplate amqpTemplate;
+        
+        @GetMapping
+        public Resp<PageVo> querySpuByCidPage(QueryCondition condition, @RequestParam(value = "catId", defaultValue = "0") Long cid) {
+                PageVo pageVo = spuInfoService.querySpuByCidPage(condition, cid);
+                
+                return Resp.ok(pageVo);
+        }
+        
+        /**
+         * 列表
+         */
+        @ApiOperation("分页查询(排序)")
+        @GetMapping("/list")
+        @PreAuthorize("hasAuthority('pms:spuinfo:list')")
+        public Resp<PageVo> list(QueryCondition queryCondition) {
+                PageVo page = spuInfoService.queryPage(queryCondition);
+                
+                return Resp.ok(page);
+        }
+        
+        /**
+         * 列表
+         */
+        @PostMapping("/page")
+        public Resp<List<SpuInfoEntity>> querySpuPage(@RequestBody QueryCondition queryCondition) {
+                PageVo page = spuInfoService.queryPage(queryCondition);
+                
+                return Resp.ok((List<SpuInfoEntity>) page.getList());
+        }
+        
+        
+        /**
+         * 信息
+         */
+        @ApiOperation("详情查询")
+        @GetMapping("/info/{id}")
+        @PreAuthorize("hasAuthority('pms:spuinfo:info')")
+        public Resp<SpuInfoEntity> info(@PathVariable("id") Long id) {
+                SpuInfoEntity spuInfo = spuInfoService.getById(id);
+                
+                return Resp.ok(spuInfo);
+        }
+        
+        /**
+         * 保存
+         */
+        @ApiOperation("保存")
+        @PostMapping("/save")
+        @PreAuthorize("hasAuthority('pms:spuinfo:save')")
+        public Resp<Object> save(@RequestBody SpuInfoVO spuInfoVO) throws FileNotFoundException {
+                spuInfoService.bigSave(spuInfoVO);
+                
+                return Resp.ok(null);
+        }
+        
+        /**
+         * 修改
+         */
+        @ApiOperation("修改")
+        @PostMapping("/update")
+        @PreAuthorize("hasAuthority('pms:spuinfo:update')")
+        public Resp<Object> update(@RequestBody SpuInfoEntity spuInfo) {
+                spuInfoService.updateById(spuInfo);
+                this.amqpTemplate.convertAndSend("PMS-SPU-EXCHANGE","item.update",spuInfo.getId());
+                return Resp.ok(null);
+        }
+        
+        /**
+         * 删除
+         */
+        @ApiOperation("删除")
+        @PostMapping("/delete")
+        @PreAuthorize("hasAuthority('pms:spuinfo:delete')")
+        public Resp<Object> delete(@RequestBody Long[] ids) {
+                spuInfoService.removeByIds(Arrays.asList(ids));
+                
+                return Resp.ok(null);
+        }
+        
 }
